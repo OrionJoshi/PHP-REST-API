19c338f59ab2835d1d8529420ee0693078bd5532
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-17 20:33:50 +0545

    database connection  parameter added

---
+++ config/Database.php
@@ -0,0 +1,11 @@
+<?php
+    class Database{
+        //Database Parameters To Connect Database
+        private $host = 'localhost';
+        private $db_name = 'myblog';
+        private $username = 'root';
+        private $password = '';
+        private $conn;
+    }
+
+?>
