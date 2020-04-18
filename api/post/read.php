20d308f206b09c0be2a920d916ec3f675c1d4e53
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-18 20:23:44 +0545

    make instant and call read function

--- api/post/read.php
+++ api/post/read.php
@@ -10,4 +10,13 @@
     $database = new Database();
     $db = $database->connect();
 
+    //Instantiate blog post object
+    $post = new Post($db);
+
+    //Blog Post query
+    $result = $post->read();
+
+    //Get row count
+    $num = $result->rowCount();
+
 ?>
