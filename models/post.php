b1050e3a246469b6c979d54cd8b0031a86305e7b
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-18 18:24:01 +0545

    post read function added

--- models/post.php
+++ models/post.php
@@ -18,6 +18,33 @@
         public function __construct($db){
             $this->conn = $db;
         }
+
+        //GET POSTs
+        public function read(){
+            //Create a Query
+            $query = 'SELECT 
+                    c.name as category_name,
+                    p.id,
+                    p.category_id,
+                    p.title,
+                    p.body,
+                    p.author
+                    p.created_at
+                FROM
+                '. $this->table .' p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                ORDER BY
+                    p.created_at DESC';
+        
+            //Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            //Execute query
+            $stmt->execute();
+            
+            return $stmt;
+        }
     }
 
 ?>
