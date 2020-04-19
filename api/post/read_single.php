20f13ef6811018eeeab1ff501984f03729d2534f
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-19 22:28:05 +0545

    print associative array in json format

--- api/post/read_single.php
+++ api/post/read_single.php
@@ -15,4 +15,20 @@
 
     //GET ID
     $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    //GET Post
+    $post->read_single();
+
+    $post_arr = array(
+
+        'id' => $post->id,
+        'title' => $post->title,
+        'body' => $post->body,
+        'author' => $post->author,
+        'category_id' => $post->category_id,
+        'category_name' => $post->category_name
+    );
+
+    //Make JSON
+    print_r(json_encode($post_arr));
 ?>
