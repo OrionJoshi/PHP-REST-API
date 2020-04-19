0687722435018c9e5ab37590443583155e6faf68
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-19 21:27:13 +0545

    header,database connect and post istant created

---
+++ api/post/read_single.php
@@ -0,0 +1,15 @@
+<?php
+    //Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+
+    //Instantiate Database and connect to Database
+    $database = new Database();
+    $db = $database->connect();
+
+    //Instantiate blog post object
+    $post = new Post($db);
+?>
