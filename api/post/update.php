b216aa7ff79c77de918bed7ffb4d46d0fda4e9f2
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-20 18:39:58 +0545

    included database and post models

--- api/post/update.php
+++ api/post/update.php
@@ -4,4 +4,8 @@
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: PUT');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
+
 ?>
