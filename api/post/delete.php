b83965943eb19beb17bba11ff54d7792402826de
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-20 19:13:04 +0545

    Headers added for delete a post

---
+++ api/post/delete.php
@@ -0,0 +1,7 @@
+<?php
+    //Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods:DELETE');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+?>
