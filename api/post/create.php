463f1bbef5791eb619cbd7777541e8b5814b78db
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-20 18:02:49 +0545

    Check whether post created or not

--- api/post/create.php
+++ api/post/create.php
@@ -19,5 +19,20 @@
     //GET the raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
+    $post->title = $data->title;
+    $post->body = $data->body;
+    $post->author = $data->author;
+    $post->category_id = $data->category_id;
+
+    //Create Post
+    if($post->create()){
+        echo json_encode(
+            array('message' => 'Post Created')
+        );
+    }else{
+        echo json_encode(
+            array('message' => 'Post Not Created')
+        );
+    }
     
 ?>
