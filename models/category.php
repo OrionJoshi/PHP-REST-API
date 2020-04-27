e263fd57a6799457d673e6a7576ff1d7c664a953
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-27 14:36:00 +0545

    category variable added

---
+++ models/category.php
@@ -0,0 +1,13 @@
+<?php 
+    class Category{
+        //DB Stuff
+        private $conn;
+        private $table = 'categories';
+
+        //Properties
+        public $id;
+        public $name;
+        public $created_at;
+    }
+
+?>
