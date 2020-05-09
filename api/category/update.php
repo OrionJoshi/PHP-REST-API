2f77ae6ee29cf2a00719af49d24577967f118d88
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-09 18:04:58 +0545

    Database file include

--- api/category/update.php
+++ api/category/update.php
@@ -5,6 +5,6 @@
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: PUT');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
-    
 
+    include_once '../../config/Database.php';
 ?>
