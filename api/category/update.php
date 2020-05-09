89b219a93326ca96cfe8f2e15d6d99868f5f7f58
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-09 18:06:17 +0545

    Category model add

--- api/category/update.php
+++ api/category/update.php
@@ -7,4 +7,5 @@
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
+    include_once '../../models/category.php';
 ?>
