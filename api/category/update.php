9589ee127cac133117e6df55bd8695b7e4547247
khageshwor <khageshwarjoshi03@gmail.com>
2020-05-09 18:02:40 +0545

    Headers add for update category

---
+++ api/category/update.php
@@ -0,0 +1,10 @@
+<?php 
+
+    //Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    
+
+?>
